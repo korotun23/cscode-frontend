3f2ca6960249427502450de2784c3cf3eb53945a
Korotun <jo.biliec23@gmail.com>
2026-02-05 12:03:24 +0000

    Feature : implementation of Pinia (state management)

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,35 @@
+// Imports
+// Vue
 import { createApp } from 'vue'
-import App from './app/App.vue'
+
+// Router
 import router from './router'
+
+// State management
+import { createPinia } from 'pinia'
 
 // Styles
 import './sass/_global.scss'
 
-// Import fonts
+// Fonts
 import '@fontsource/poppins'
 import '@fontsource/courier-prime'
 
-// Import services
+// App
+import App from './app/App.vue'
+
+// Services
 import { BarcodeTypeService } from './services/barcodeTypeService'
 import { BarcodeGeneratorService } from './services/barcodeGeneratorService'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 // Provide services to the app and all child components
 app.provide('barcodeTypeService', new BarcodeTypeService())
 app.provide('barcodeGeneratorService', new BarcodeGeneratorService())
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
